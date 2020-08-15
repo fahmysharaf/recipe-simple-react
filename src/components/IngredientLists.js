f00522a3ace509ef1560f3d86bbbab88fc211997
fahmy <fahmy@localhost.localdomain>
2020-08-14 21:13:21 -0400

    Recipe Add and Delete Button Interactivity

---
+++ src/components/IngredientLists.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Ingredient from './Ingredient'
+export default function IngredientLists({ingredients}) {
+    const ingredientElements = ingredients.map( ingredient => {
+        return <Ingredient key={ingredient.id} {...ingredient}/> 
+    })
+    return (
+        <div>
+            {ingredientElements}
+        </div>
+    )
+}
